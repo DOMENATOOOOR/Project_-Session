d61b2c488e2b9afa8314b23438bfd8d7ad1e443a
Pan_Sasha <pankov.aleksander05@gmail.com>
2025-06-22 14:48:53 +0700

    Update board.h

--- board.h
+++ board.h
@@ -9,9 +9,10 @@ public:
     bool isGameOver() const;
     char checkWinner() const;  // 'X', 'O' или ' ' (ничья)
     bool isDraw() const;
-    const std::vector<std::vector<char>>& getGrid() const;
+    const std::vector<char>& getGrid() const;
     void reset();
 
 private:
-    std::vector<std::vector<char>> grid;
+    std::vector<char> grid;
+    int toIndex(int x, int y) const { return x * 3 + y; }  // Преобразование 2D в 1D
 };
